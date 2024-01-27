f1ead5bc2e6060a89511287750fd783b66696062
Tushar.garggg <tushargarg9868@gmail.com>
2024-01-28 00:12:16 +0530

    step 1

---
+++ p1/src/components/Hero.jsx
@@ -0,0 +1,29 @@
+
+const HeroSection = () => {
+    return (
+        <main className="hero conatiner">
+            <div className="hero-content">
+                <h1>YOUR FEET DESERVE THE BEST</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et voluptatem nam, doloremque necessitatibus dicta eligendi veritatis id velit provident reiciendis?</p>
+                <div className="hero-btn">
+                    <button>Shop now </button>
+                    <button>catogery</button>
+                </div>
+                <div className="shopping">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, modi.
+                    </p>
+                </div>
+                <div className="brand-icons">
+                    <img src="/images/flipkart.png" alt="" srcset="" />
+                    <img src="/images/flipkart.png" alt="" srcset="" />
+                </div>
+            </div>
+            <div className="hero-image">
+                <img src="/images/hero-image.png" alt="" srcset="" />
+            </div>
+
+        </main>
+    )
+}
+
+export default HeroSection
---
+++ p1/src/components/Navigation.jsx
@@ -0,0 +1,18 @@
+const Navigation = () => {
+    return (
+        <nav className="container">
+            <div className="logo">
+                <img src="/images/brand_logo.png" alt="logo" srcset="" />
+            </div>
+            <ul>
+                <li herf="#">Menu</li>
+                <li herf="#">Location</li>
+                <li herf="#">About</li>
+                <li herf="#">Conatct</li>
+            </ul>
+            <button> login</button>
+        </nav>
+    )
+}
+
+export default Navigation
